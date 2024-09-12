807456691d3a4611bbafa5631f7d37369d5f7b75
vitorraiaa <vitortraia@gmail.com>
2024-09-12 11:58:36 -0300

    A

--- camera/camera.py
+++ camera/camera.py
@@ -3,12 +3,9 @@ import cv2 as cv
 import math
 
 def rotacionar(imagem, angulo):
-
     (altura, largura) = imagem.shape[:2]
 
-
     centro = (largura / 2, altura / 2)
-
 
     cos_theta = math.cos(angulo)
     sin_theta = math.sin(angulo)
@@ -17,43 +14,57 @@ def rotacionar(imagem, angulo):
         [sin_theta, cos_theta]
     ])
 
-
     y_indices, x_indices = np.indices((altura, largura))
-
     coordenadas = np.stack((x_indices.ravel(), y_indices.ravel()), axis=0)
 
-
     coordenadas_centradas = coordenadas - np.array([[centro[0]], [centro[1]]])
-
-
     coordenadas_rotacionadas = R @ coordenadas_centradas
-
-
     coordenadas_final = coordenadas_rotacionadas + np.array([[centro[0]], [centro[1]]])
-
 
     x_rot = coordenadas_final[0, :].reshape((altura, largura)).astype(int)
     y_rot = coordenadas_final[1, :].reshape((altura, largura)).astype(int)
 
-
     imagem_rotacionada = np.zeros_like(imagem)
-
 
     valid_mask = (
         (x_rot >= 0) & (x_rot < largura) &
         (y_rot >= 0) & (y_rot < altura)
     )
 
-
     imagem_rotacionada[y_indices[valid_mask], x_indices[valid_mask]] = \
         imagem[y_rot[valid_mask], x_rot[valid_mask]]
 
     return imagem_rotacionada
 
+def cisalhar(imagem, fator_cisalhamento):
+    (altura, largura) = imagem.shape[:2]
+    S = np.array([
+        [1, fator_cisalhamento],
+        [0, 1]
+    ])
+
+    y_indices, x_indices = np.indices((altura, largura))
+    coordenadas = np.stack((x_indices.ravel(), y_indices.ravel()), axis=0)
+
+    coordenadas_cis = S @ coordenadas
+
+    x_cis = coordenadas_cis[0, :].reshape((altura, largura)).astype(int)
+    y_cis = coordenadas_cis[1, :].reshape((altura, largura)).astype(int)
+
+    imagem_cisalhada = np.zeros_like(imagem)
+
+    valid_mask = (
+        (x_cis >= 0) & (x_cis < largura) &
+        (y_cis >= 0) & (y_cis < altura)
+    )
+
+    imagem_cisalhada[y_indices[valid_mask], x_indices[valid_mask]] = \
+        imagem[y_cis[valid_mask], x_cis[valid_mask]]
+
+    return imagem_cisalhada
+
 def run():
-
     cap = cv.VideoCapture(0)
-
 
     width = 320
     height = 240
@@ -63,6 +74,8 @@ def run():
         exit()
 
     angulo = 0
+    velocidade = 1  
+    fator_cisalhamento = 0.0  
 
     while True:
         ret, frame = cap.read()
@@ -74,20 +87,29 @@ def run():
         frame = cv.resize(frame, (width, height), interpolation=cv.INTER_AREA)
         image = frame.astype(float) / 255.0
 
-
         angulo_rad = np.radians(angulo)
-
         rotacionada = rotacionar(image, angulo_rad)
 
-        cv.imshow('Video Rotacionado', rotacionada)
 
-        angulo = (angulo + 1) % 360  
+        imagem_final = cisalhar(rotacionada, fator_cisalhamento)
 
-        if cv.waitKey(1) == ord('q'):
+        cv.imshow('Video Transformado', imagem_final)
+
+        angulo = (angulo + velocidade) % 360  
+
+        key = cv.waitKey(1)
+        if key == ord('q'):
             break
+        elif key == ord('w'):  
+            velocidade += 1
+        elif key == ord('s'): 
+            velocidade = max(1, velocidade - 1)
+        elif key == ord('d'):  
+            fator_cisalhamento += 0.1
+        elif key == ord('a'):  
+            fator_cisalhamento -= 0.1
 
     cap.release()
     cv.destroyAllWindows()
-
 
 run()
